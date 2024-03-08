38718d1148d2d270114b19c964972de054056d33
Wei Su <suv@cyberconnect.me>
2024-03-08 23:58:12 +0800

    fix: add favicon

--- theme.config.jsx
+++ theme.config.jsx
@@ -26,6 +26,7 @@ export default {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta property="og:title" content="CyberConnect" />
       <meta property="og:description" content="CyberConnect Developer Docs" />
+      <link rel="icon" href="/assets/favicon.ico" type="image/x-icon" />
     </>
   ),
   footer: {
