6b3707db31e5c27d5e738643920d0d7d7ae9b57f
0xpranjal.eth <50989807+0xpranjal@users.noreply.github.com>
2024-07-02 16:12:35 +0530

    usecases updates + thirdweb sdk tutorial (#24)

--- vocs.config.ts
+++ vocs.config.ts
@@ -155,6 +155,9 @@ export default defineConfig({
           {
             text:"Deploying a token using Thirdweb", link: "/build-on-cyber/thirdweb-cli"
           },
+          {
+            text: "Building a dApp using Thirdweb SDK", link: "/build-on-cyber/thirdweb-sdk"
+          },
         ] 
       },
 
@@ -363,7 +366,7 @@ export default defineConfig({
     ],
   },
   topNav: [
-    { text: "Overview", link: "https://docs.cyber.co/" },
+    { text: "Overview", link: "/", match: "/index" },
     { text: "Build on Cyber", link: "/build-on-cyber/network-info" },
     { text: "SDKs", link: "/sdk/cyber-account" },
     { text: "CyberWallet", link: "/cyber-wallet/overview" },
